330827b2fd73352540ff9ddf1e7c0bf6612cb525
thanawat88 <toon1072@hotmail.com>
2022-03-25 14:33:51 +0700

    second commid

--- src/testhello/TestHello.java
+++ src/testhello/TestHello.java
@@ -15,6 +15,7 @@ public class TestHello {
      */
     public static void main(String[] args) {
         System.out.println("Hello, World");
+        System.out.println("Welcome to Thailand");
     }
     
 }
